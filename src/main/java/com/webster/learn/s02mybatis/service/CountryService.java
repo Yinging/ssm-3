70a6b3b874345e688f9c0799101636f8cbaa8fcb
webster13 <webster13@163.com>
2016-03-10 23:04:52 +0800

    mbg 的通用mapper配置,未完成

---
+++ src/main/java/com/webster/learn/s02mybatis/service/CountryService.java
@@ -0,0 +1,13 @@
+package com.webster.learn.s02mybatis.service;
+
+import com.webster.learn.s02mybatis.entity.Country;
+import com.webster.learn.s02mybatis.mapper.CountryMapper;
+import org.springframework.stereotype.Service;
+
+/**
+ * todo
+ * Created by Webster on 16/3/10.
+ */
+@Service
+public class CountryService extends BaseService<CountryMapper,Country> {
+}
---
+++ src/test/java/com/webster/learn/s01mvc/BaseTest.java
@@ -0,0 +1,16 @@
+package com.webster.learn.s01mvc;
+
+/**
+ * todo
+ * Created by Webster on 16/3/10.
+ */
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring.xml")
+public class BaseTest {
+}
+
